ced996e69c9b4862d83ca4e054b3234b44f11154
Dennis Lehmann <delema98@gmail.com>
2022-03-27 19:36:20 +0200

    implement toastr alerts when ereate/edit/delete successful

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace mvc_tutorial.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -77,6 +78,8 @@ namespace mvc_tutorial.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -117,6 +120,8 @@ namespace mvc_tutorial.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
             return RedirectToAction("Index");
         }
     }
